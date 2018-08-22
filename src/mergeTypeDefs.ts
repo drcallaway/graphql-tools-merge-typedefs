3f82ba987bc68714e7d0086b6abfd946936ac16a
Tyler Benton <tjbenton21@gmail.com>
2018-08-22 10:21:36 -0400

    updated various things
    moved tests to a top level `test` folder
    updated all tests to use `graphql-tag` and snapshots of the results to make it easier for other developers to see how this library can be used and what the expected result is.
    
    updated a few devDependencies
    
    updates to the merge function
      - filters out falsy values
      - removes the type definition if it's found in the definitions list
      - moved some of the logic up to reduce extra loops
      - filters out duplicate keys. The last key always wins just like other extend functions

--- src/mergeTypeDefs.ts
+++ src/mergeTypeDefs.ts
@@ -7,42 +7,50 @@ import { ITypeDefinitions } from 'graphql-tools';
  * @param type Operation type to merge
  */
 function merge(defs: ITypeDefinitions, type: String): ITypeDefinitions {
-  let operations = [];
+  let result;
 
   defs = (defs as any).filter(def => {
-    const definitions = def.definitions.filter(definition => {
-      const name = definition.name;
-      if (definition.kind === 'ObjectTypeDefinition' && name.kind === 'Name' && name.value === type) {
-        operations.push(definition);
+    // filter out falsy values just incase
+    if (!def) return false;
+
+    // reset def.definitions to be the filtered version so the `type` that
+    // was passed in doesn't exist in it anymore
+    def.definitions = def.definitions.filter(definition => {
+      const { kind, name, fields } = definition;
+      if (kind === 'ObjectTypeDefinition' && name.kind === 'Name' && name.value === type) {
+        if (!result) {
+          result = definition;
+        } else {
+          result.fields = result.fields.concat(fields);
+        }
+
+        // remove the definition because it's been added to the result and
+        // shouldn't exist here anymore
         return false;
       }
 
       return true;
     });
 
-    if (definitions.length === 0) {
+    if (def.definitions.length === 0) {
       return false; // operation was the only definition so this can be removed
     }
 
     return true;
   });
 
-  if (operations.length > 0) {
-    if (operations.length > 1) {
-      operations = operations.reduce((acc, query) => {
-        if (acc === undefined) {
-          acc = query;
-        } else {
-          acc.fields = acc.fields.concat(query.fields);
-        }
-
-        return acc;
+  // update to be `result?.fields?.length` when it's available
+  if (((result || {}).fields || []).length) {
+    if (result.fields.length > 1) {
+      // filters out duplicate keys. The last key always wins
+      result.fields = result.fields.filter((field, i, array) => {
+        return !array.slice(i + 1).find((obj) => obj.name.value === field.name.value);
       });
     }
 
     (defs as any).push({
       kind: "Document",
-      definitions: [operations]
+      definitions: [result],
     });
   }
 
---
+++ test/mergeTypeDefs.test.ts
@@ -0,0 +1,116 @@
+import gql from 'graphql-tag'
+import { snapshot } from './utils';
+import { mergeTypeDefs, merge } from '../src/mergeTypeDefs';
+
+
+test.each([ 'Query', 'Mutation', 'Subscription' ])('%s type defs are merged', (type) => {
+  const defs: any = mergeTypeDefs([
+    gql`
+      type ${type} {
+        ${type.toLowerCase()}Field1: String
+      }
+    `,
+    gql`
+      type ${type} {
+        ${type.toLowerCase()}Field2: String
+      }
+    `,
+  ])
+
+  snapshot(defs)
+})
+
+
+test('all type defs are merged', () => {
+  const defs: any = mergeTypeDefs([
+    gql`
+      type Query {
+        queryFieldSet1: String
+      }
+      type Mutation {
+        mutationFieldSet1: String
+      }
+      type Subscription {
+        subscriptionFieldSet1: String
+      }
+    `,
+    gql`
+      type Query {
+        queryFieldSet2: String
+      }
+      type Mutation {
+        mutationFieldSet2: String
+      }
+      type Subscription {
+        subscriptionFieldSet2: String
+      }
+    `
+  ])
+
+  snapshot(defs)
+});
+
+test('two fields in the same type', () => {
+  const defs: any = merge([
+    gql`
+      type Local {
+        field1: String
+        field2: String
+      }
+    `,
+    gql`
+      type Local {
+        field1: Int
+      }
+    `,
+  ], 'Local')
+
+  snapshot(defs)
+})
+
+
+test('multiple types in a def', () => {
+  const defs: any = merge([
+    gql`
+      type Fun {
+        woohoo: String
+      }
+      type User {
+        id: String
+      }
+      type Local {
+        field1: User
+        field2: Fun
+      }
+    `,
+    gql`
+      type Local {
+        field3: Int
+      }
+    `,
+  ], 'Local')
+
+  // make sure the type is removed from each def except the last one
+  expect(defs.slice(0, -1).find((def) => def.definitions.find(({ name }) => name.value === 'Local'))).toBeFalsy()
+  snapshot(defs)
+})
+
+test('filters out falsy values', () => {
+  const defs: any = merge([
+    false,
+    null,
+    gql`
+      type Something {
+        field1: Int
+      }
+    `,
+    gql`
+      type Query {
+        something: Something
+      }
+    `,
+  ], 'Something')
+
+  // make sure the type is removed from each def except the last one
+  snapshot(defs)
+})
